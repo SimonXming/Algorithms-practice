2bced10ec8c4bf02f21398ea2e7dab8cfe2b7f1d
SimonXming <simon_xu@outlook.com>
2017-06-24 14:26:55 +0800

    Add insertion sort.

--- 2/2.1/insertion_sort.go
+++ 2/2.1/insertion_sort.go
@@ -1,6 +1,17 @@
 /*
-选择排序
-对于长度为 N 的数组，选择排序需要大约 N^2/2 次比较和 N 次交换。
+插入排序
+对于随机排列的长度 n 且主键不重复的数组：
+平均情况下插入排序需要 n^2/4 次比较和 n^2/4 次交换；
+最坏情况 n^2/2 次比较和 n^2/2 次交换；
+最好情况 n-1 次比较和 0 次交换。
+
+插入排序对部分有序的数组很有效：
+* 数组中每个元素距离它的最终位置都不远
+* 一个有序的大数组接一个小数组
+* 数组中只有几个元素的位置不正确
+
+事实上，当倒置的元素很少时，插入排序很可能比本章中其他任何算法都快。
+
 */
 package main
 
@@ -18,6 +29,9 @@ type CharSlice []string
 func (c *CharSlice) Len() int {
 	return len(*c)
 }
+func (c *CharSlice) RigheSwap(i, j int) {
+	(*c)[i], (*c)[j] = (*c)[j], (*c)[i]
+}
 func (c *CharSlice) Swap(i, j int) {
 	(*c)[i], (*c)[j] = (*c)[j], (*c)[i]
 }
@@ -29,21 +43,19 @@ func (c *CharSlice) Show() {
 	fmt.Printf("%v\n", *c)
 }
 
-func (c *CharSlice) SelectionSort() {
+func (c *CharSlice) InsertionSort() {
 	// 获取待排序的列表长度
 	length := c.Len()
 	// 逐次遍历列表
 	for i := 0; i < length; i++ {
-		// 初始化最小值 min => i
-		min := i
-		// 从 i 遍历到结尾
-		// 找到最小的元素 min
-		// 把 min 元素和 i 位置元素互换位置
-		for j := i + 1; j < length; j++ {
-			if c.Less(j, min) {
-				min = j
-			}
-			c.Swap(i, min)
+		// 设置标记位元素, 标记位元素左侧是已经排序的，标记位元素右侧是未排序的
+		// 每次标记位右移，就是引入一个新的未排序元素 j，并在已经排序的元素中找一个合适的位置插入
+		for j := i; j > 0 && c.Less(j, j-1); j-- {
+			// 满足上述时，j 元素和前面的元素交换一下位置
+			// 直到不满足条件，j 元素此时已处于已排序的位置。
+			c.Swap(j, j-1)
+			// 要大幅提高插入排序的速度，只需要在内循环中将较大的元素向右移动而不是交换位置
+			// 这样访问数组的次数就能减半
 		}
 	}
 }
@@ -81,6 +93,6 @@ func main() {
 	filePath := filepath.Join(dataBase, "21elementary", "tiny.txt")
 	// filePath := filepath.Join(dataBase, "21elementary", "tiny.txt")
 	s_slict := newCollectionFromFile(filePath)
-	s_slict.SelectionSort()
+	s_slict.InsertionSort()
 	s_slict.Show()
 }
--- playground.go
+++ playground.go
@@ -2,32 +2,8 @@ package main
 
 import "fmt"
 
-// Parent Object.
-type Object struct{}
-
-func (b Object) String() string {
-	return "I am an Object"
-}
-
-type ObjectOne struct {
-	Object
-}
-
-type ObjectTwo struct {
-	Object
-}
-
-func (b ObjectTwo) String() string {
-	return "I am an Object Two"
-}
-
 func main() {
-	o := Object{}
-	fmt.Println(o) // I am an Object
-
-	o1 := ObjectOne{}
-	fmt.Println(o1) // I am an Object
-
-	o2 := ObjectTwo{}
-	fmt.Println(o2) // I am an Object Two
+	a := []int{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	x, a := a[0], a[1:]
+	fmt.Println(a, x)
 }
